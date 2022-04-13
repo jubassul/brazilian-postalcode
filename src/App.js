bc33202e326cbf82ec7353e1d5239fcd9d550f9e
Júlia Bassul <jubassul@gmail.com>
2022-04-13 17:03:18 -0300

    add useState in the input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import "./styles.css";
 function App() {
+  const [input, setInput] = useState("");
+
+  function handleSearch() {
+    alert(" valor do input " + input);
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador de Cep</h1>
       <div className="containerSearch">
-        <input type="text" placeholder="Digite o seu Cep" />
-        <button className="button">
+        <input
+          type="text"
+          placeholder="Digite o seu Cep"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button className="button" onClick={handleSearch}>
           <FiSearch size={25} color="#8eb695" />
         </button>
       </div>
