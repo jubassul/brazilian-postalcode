428c8ec5196af335db4249ca21b4019df87d1fda
jubassul <jubassul@gmail.com>
2023-01-10 14:47:17 +0000

    update layout

--- src/App.js
+++ src/App.js
@@ -2,110 +2,139 @@ import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import "./styles.css";
 import api from "./services/api";
+import Img from "./img01.svg";
+import Icon from "./icon01.svg";
 function App() {
-  const [input, setInput] = useState(""); // busca cep
-  const [cep, setCep] = useState({}); //renderizar enderecos na tela
+  const [inputCep, setInputCep] = useState("");
   const [inputUf, setInputUf] = useState("");
   const [inputCity, setInputCity] = useState("");
-  const [inputAddress, setInputAdress] = useState("");
-  const [searchCep, setSearchCep] = useState([]); //renderizar ceps encontrados na tela
+  const [inputAddress, setInputAddress] = useState("");
 
-  async function handleSearch() {
-    if (input === "") {
-      alert("Preencha algum cep");
+  const [searchAddress, setSearchAddress] = useState({}); //render address
+  const [searchCep, setSearchCep] = useState([]); //render cep
+
+  const [errorCep, setErrorCep] = useState(false);
+  const [errorAddress, setErrorAddress] = useState(false);
+  const [notFoundAddress, setNotFoundAddress] = useState(false);
+  const [notFoundCep, setNotFoundCep] = useState(false);
+
+  async function handleSearchAddress() {
+    if (inputCep === "") {
+      setErrorAddress(true);
       return;
     }
-
     try {
-      const response = await api.get(`${input}/json`);
-      console.log(response.data);
-      setCep(response.data);
+      const responseSearchAddress = await api.get(`${inputCep}/json`);
+      setSearchAddress(responseSearchAddress.data);
     } catch {
-      alert("Ops, erro ao buscar");
-      setInput("");
+      setNotFoundAddress(true);
+      setInputCep("");
     }
   }
   async function handleSearchCep() {
     if (inputUf === "" || inputAddress === "" || inputCity === "") {
-      alert("Prencha os campos ");
+      setErrorCep(true);
       return;
     }
     try {
       const responseSearchCep = await api.get(
         `${inputUf}/${inputCity}/${inputAddress}/json`
       );
-      console.log(responseSearchCep.data);
       setSearchCep(responseSearchCep.data);
     } catch {
-      alert("Erro");
+      setNotFoundCep(true);
       setInputUf("");
-      setInputAdress("");
+      setInputAddress("");
       setInputCity("");
     }
   }
 
   return (
     <div className="container">
-      <h1 className="title">Buscador de Cep</h1>
-      <div className="containerSearch">
-        <input
-          type="text"
-          placeholder="Digite o seu Cep"
-          value={input}
-          onChange={(event) => setInput(event.target.value)}
-        />
-        <button className="button" onClick={handleSearch}>
-          <FiSearch size={25} color="#4e639e" />
-        </button>
-      </div>
-      <div>
-        <div className="containerSearch">
+      <header>
+        <div className="header">
+          <h1 className="title">Brazil Postal Code</h1>
+          <img src={Icon} className="icon" />
+        </div>
+        <p className="subtitle">
+          Search brazilian postal code anywhere in the world
+        </p>
+      </header>
+
+      <div className="content">
+        <div className="containerSearchAddress">
+          <h2>Search the address</h2>
+          <div className="search-address">
+            <input
+              type="text"
+              placeholder="Type the postal code"
+              value={inputCep}
+              onChange={(event) => setInputCep(event.target.value)}
+              className="input-search-address"
+            />
+            <button className="button-address" onClick={handleSearchAddress}>
+              <FiSearch className="search-img" size={25} color="#AA14F0" />
+            </button>
+          </div>
+          {errorAddress ? <span>Preencha algum campo</span> : ""}
+          {notFoundAddress ? <span>Não Encontrado</span> : ""}
+          <img src={Img} />
+        </div>
+        <div className="containerSearchCep">
+          <h2>Search the postal code</h2>
           <input
             type="text"
-            placeholder="Digite o seu UF"
+            placeholder="Type the UF"
             value={inputUf}
             onChange={(event) => setInputUf(event.target.value)}
+            className="input-postal-code"
           />
-        </div>
-        <div className="containerSearch">
+
           <input
             type="text"
-            placeholder="Digite a sua Cidade"
+            placeholder="Type the city"
             value={inputCity}
             onChange={(event) => setInputCity(event.target.value)}
+            className="input-postal-code"
           />
-        </div>
-        <div className="containerSearch">
+
           <input
             type="text"
-            placeholder="Digite o seu Logradouro"
+            placeholder="Type the address"
             value={inputAddress}
-            onChange={(event) => setInputAdress(event.target.value)}
+            onChange={(event) => setInputAddress(event.target.value)}
+            className="input-postal-code"
           />
+          <button className="button-postal-code" onClick={handleSearchCep}>
+            Search
+          </button>
+          {errorCep ? <span>Preencha algum campo</span> : ""}
+          {notFoundCep ? <span>Não Encontrado</span> : ""}
         </div>
-        <button className="button" onClick={handleSearchCep}>
-          <FiSearch size={25} color="#4e639e" />
-        </button>
       </div>
-      {Object.keys(cep).length > 0 && (
-        <div className="main">
-          <h2>CEP: {cep.cep}</h2>
-          <span>Rua: {cep.logradouro}</span>
-          <span>Complemento: {cep.complemento}</span>
-          <span>Bairro: {cep.bairro}</span>
-          <span>
-            {cep.localidade} - {cep.uf}
-          </span>
+      {Object.keys(searchAddress).length > 0 && (
+        <div className="content-main">
+          <div className="main">
+            <h2>CEP: {searchAddress.cep}</h2>
+            <span>Rua: {searchAddress.logradouro}</span>
+            <span>Complemento: {searchAddress.complemento}</span>
+            <span>Bairro: {searchAddress.bairro}</span>
+            <span>
+              {searchAddress.localidade} - {searchAddress.uf}
+            </span>
+          </div>
         </div>
       )}
       {Object.keys(searchCep).length > 0 && (
-        <div className="main">
-          <h2>Ceps Encontrados:</h2>
-          <ul>
-            {searchCep.map((searchCeps) => (
-              <li key={searchCeps.cep}>{searchCeps.cep}</li>
-            ))}
-          </ul>
+        <div className="content-main">
+          <div className="main">
+            <h2>Ceps Encontrados:</h2>
+            <ul>
+              {searchCep.map((searchCeps) => (
+                <li key={searchCeps.cep}>{searchCeps.cep}</li>
+              ))}
+            </ul>
+          </div>
         </div>
       )}
     </div>
